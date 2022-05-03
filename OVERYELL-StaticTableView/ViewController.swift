6b0aa4124b5bc3cebd943ae60303fe6b6ed80207
Benjamin Skomrock <ben@overyell.com>
2022-05-03 09:13:17 +0200

    Initial Commit

---
+++ OVERYELL-StaticTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OVERYELL-StaticTableView
+//
+//  Created by Benjamin Skomrock on 03.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
